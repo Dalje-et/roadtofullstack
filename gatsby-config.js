e6def3691ef378901a1dcb1b197b8a5ccc6b1e37
Daljeet Sandu <daljeet-sandu@hotmail.de>
2020-11-23 16:52:03 +0100

    clean up assets

--- gatsby-config.js
+++ gatsby-config.js
@@ -153,14 +153,14 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'Gatsby Starter Blog',
-        short_name: 'GatsbyJS',
+        name: 'Road to Fullstack',
+        short_name: 'RoadToFullStack',
         start_url: '/',
         background_color: '#ffffff',
         theme_color: '#663399',
         display: 'minimal-ui',
         // edit below
-        icon: 'content/assets/gatsby-icon.png'
+        icon: 'content/assets/logo.png'
       }
     },
     {
